5e8b943fce6081bea658023ac709aa98efbd0187
Анна Тодосюк <antodospost@gmail.com>
2024-05-01 11:29:10 -0500

    change

---
+++ src/test/java/tests/ConflictTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ConflictTests {
+    @Test
+    public void test(){
+        int a = 2;
+        int b = 3;
+        System.out.println("Sum" + (a+5));
+
+    }
+    @Test
+    public void test2(){
+        String message = "Welcome ti USA";
+        System.out.println(message);;
+    }
+}
